9252a62334563840a553535f63541f5a79597f3a
姜林升 <1257892551@qq.com>
2021-06-07 16:30:41 +0800

    feat(basic):添加正则表达式代码

--- basic/src/main/java/com/jiangls/regexp/RegExp.java
+++ basic/src/main/java/com/jiangls/regexp/RegExp.java
@@ -5,13 +5,13 @@ import java.util.List;
 
 public class RegExp {
     public static void main(String[] args) {
-        String source = "</p>4688888888</p>\n" +
-                "<figure class=\"image image_resized image-style-align-left\" style=\"width:50%;\"><img src=\"/api/mailTemplate/imgDownload?id=85c0a71187d1427c9af354b96e235a43\"></figure>\n" +
-                "<figure class=\"image image_resized image-style-align-right\" style=\"width:50%;\"><img src=\"/api/mailTemplate/imgDownload?id=994bfde9d40f432eb82954bc4cdc4029\"></figure>";
-        List<String> idList = Arrays.asList("85c0a71187d1427c9af354b96e235a43", "994bfde9d40f432eb82954bc4cdc4029");
+        String source = "<p>123332543234</p>\n" +
+                "<figure class=\"image image-style-align-left image_resized\" style=\"width:50%;\"><img src=\"/api/mailTemplate/imgDownload?id=92bb79cc5774481ea3a05eed599b3796\"></figure>\n" +
+                "<figure class=\"image image_resized image-style-align-right\" style=\"width:50%;\"><img src=\"/api/mailTemplate/imgDownload?id=a2212328801348459e6144221a705330\"></figure>";
+        List<String> idList = Arrays.asList("a2212328801348459e6144221a705330", "92bb79cc5774481ea3a05eed599b3796");
 
         for(String id : idList) {
-            String reg = "<img[\\W]*src=[\'\"][/].+?\\?id=" + id + "[\'\"]>";
+            String reg = "<img[\\W]*src=[\'\"][a-zA-Z0-9/]+\\?id=" + id + "[\'\"]>";
             String replacement = "<img src='cid:" + id + "'>";
             source = source.replaceAll(reg, replacement);
         }
