285f2211c09a833b35ef5ab49abf229a3ab21f7f
姜林升 <1257892551@qq.com>
2021-07-13 09:06:23 +0800

    feat(basic):添加typecast包

---
+++ basic/src/main/java/com/jiangls/typecast/TypeCast.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2015—2030 GantSoftware.Co.Ltd. All rights reserved.
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * is not allowed to be distributed or copied without the license from
+ * GantSoftware.Co.Ltd. Please contact the company for more information.
+ */
+package com.jiangls.typecast;
+
+/**
+ * @Author jiangls
+ * @Date 2021/7/12
+ */
+public class TypeCast {
+    public static void main(String[] args) {
+        String str = (String) null;
+        System.out.println(str);
+    }
+}
