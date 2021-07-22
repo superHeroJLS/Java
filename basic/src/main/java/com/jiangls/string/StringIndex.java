108f9ce793a6628a4b8e2f78a2bc8abb11ae40a2
姜林升 <1257892551@qq.com>
2021-07-22 16:34:04 +0800

    feat(basic):添加string包

---
+++ basic/src/main/java/com/jiangls/string/StringIndex.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2015—2030 GantSoftware.Co.Ltd. All rights reserved.
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * is not allowed to be distributed or copied without the license from
+ * GantSoftware.Co.Ltd. Please contact the company for more information.
+ */
+package com.jiangls.string;
+
+/**
+ * @Author jiangls
+ * @Date 2021/7/22
+ */
+public class StringIndex {
+    public static void main(String[] args) {
+        System.out.println("123".indexOf("23"));
+    }
+}
