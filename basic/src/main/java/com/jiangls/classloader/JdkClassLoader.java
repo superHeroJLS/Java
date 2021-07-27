b96985c640fc5a049209da43297f48d3563e4090
姜林升 <1257892551@qq.com>
2021-07-27 14:25:07 +0800

    feat(basic):添加JdkClassLoader

--- basic/src/main/java/com/jiangls/classloader/JdkClassLoader.java
+++ basic/src/main/java/com/jiangls/classloader/JdkClassLoader.java
@@ -7,26 +7,24 @@
  */
 package com.jiangls.classloader;
 
-import sun.misc.Launcher;
-
 /**
  * @Author jiangls
  * @Date 2021/7/27
  */
-public class JdkClassLocader {
+public class JdkClassLoader {
     public static void main(String[] args) {
 
         // JdkClassLocader类加载器为Launcher$AppClassLoader
-        System.out.println(JdkClassLocader.class.getClassLoader());
+        System.out.println(JdkClassLoader.class.getClassLoader());
 
         // Launcher$AppClassLoader类加载器为BootstrapClassLoader，因为BootstrapClassLoader不是Java代码实现的，所以打印出来的是null
-        System.out.println(JdkClassLocader.class.getClassLoader().getClass().getClassLoader());
+        System.out.println(JdkClassLoader.class.getClassLoader().getClass().getClassLoader());
 
         // Launcher$AppClassLoader的父加载器是Launcher$ExtClassLoader
-        System.out.println(JdkClassLocader.class.getClassLoader().getParent());
+        System.out.println(JdkClassLoader.class.getClassLoader().getParent());
 
         // Launcher$ExtClassLoader类加载器为BootstrapClassLoader，因为BootstrapClassLoader不是Java代码实现的，所以打印出来的是null
-        System.out.println(JdkClassLocader.class.getClassLoader().getParent().getClass().getClassLoader());
+        System.out.println(JdkClassLoader.class.getClassLoader().getParent().getClass().getClassLoader());
 
     }
 }
