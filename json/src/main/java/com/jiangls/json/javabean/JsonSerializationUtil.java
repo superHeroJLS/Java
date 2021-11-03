b00ffc4b4f2a2ffb0dc6ddb38b5bfc7654e1c8ad
superHeroJLS <1257892551@qq.com>
2021-11-03 18:13:06 +0800

    feat(json):添加Class JsonSerializationUtil

---
+++ json/src/main/java/com/jiangls/json/javabean/JsonSerializationUtil.java
@@ -0,0 +1,10 @@
+package com.jiangls.json.javabean;
+
+/**
+ * <p>JSON序列化和反序列化工具</p>
+ * 参考链接：https://www.liaoxuefeng.com/wiki/1252599548343744/1320418650619938
+ */
+public class JsonSerializationUtil {
+
+
+}
