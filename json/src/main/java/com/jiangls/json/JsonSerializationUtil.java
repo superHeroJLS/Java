b63fa73d3d9cecbb0febe406de0834a66f1dc0a4
superHeroJLS <1257892551@qq.com>
2021-11-04 09:12:17 +0800

    feat(json):JsonSerializationUtil代码更新

--- json/src/main/java/com/jiangls/json/JsonSerializationUtil.java
+++ json/src/main/java/com/jiangls/json/JsonSerializationUtil.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.jiangls.json.javabean.Book;
+import com.jiangls.json.jsondeserializer.IsbnDeserializer;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -26,17 +27,19 @@ public class JsonSerializationUtil {
 
     public static void main(String[] args) throws IOException {
         demo();
+        demoOfDataTypeJsr310();
+        demoOfIsbnDeserializer();
     }
 
     /**
      * 将json字符串反序列化成java对象，将java对象序列化成json字符串
-     *
      */
     public static void demo() throws IOException {
         InputStream input = JsonSerializationUtil.class.getResourceAsStream("/book.json");
 
         ObjectMapper mapper = new ObjectMapper();
         mapper.registerModule(new JavaTimeModule());
+
         // 反序列化是忽略不存在的JavaBean属性
         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         Book book = mapper.readValue(input, Book.class);
@@ -45,35 +48,71 @@ public class JsonSerializationUtil {
         // 序列化
         String jsonStr = mapper.writeValueAsString(book);
         System.out.println(jsonStr);
+    }
 
 
-        /*
-        Jackson反序列化特定Java对象，比如LocalDate类型，只需要引入标准的JSR 310关于JavaTime的数据格式定义至Maven
-        com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.10.0
-        创建ObjectMapper时，注册一个新的JavaTimeModule
-         */
+    /**
+     * Jackson反序列化特定Java对象，比如LocalDate类型，只需要引入标准的JSR 310关于JavaTime的数据格式定义至Maven<br/>
+     * com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.10.0<br/>
+     *
+     * <b>注意在创建ObjectMapper时，注册一个新的JavaTimeModule</b>
+     * @throws IOException
+     */
+    public static void demoOfDataTypeJsr310() throws IOException {
+        InputStream input = JsonSerializationUtil.class.getResourceAsStream("/book.json");
 
+        // 创建ObjectMapper时，注册一个新的JavaTimeModule
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
 
-        /*
-        有些时候，内置的解析规则和扩展的解析规则如果都不满足我们的需求，还可以自定义解析。
-        举个例子，假设Book类的isbn是一个BigInteger
+        // 反序列化是忽略不存在的JavaBean属性
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        Book book = mapper.readValue(input, Book.class);
+        System.out.println(book.toString());
 
-        public class Book {
-            public String name;
-            public BigInteger isbn;
-        }
+        // 序列化
+        String jsonStr = mapper.writeValueAsString(book);
+        System.out.println(jsonStr);
+    }
 
-        但JSON数据并不是标准的整形格式：
+    /**
+     * 有些时候，内置的解析规则和扩展的解析规则如果都不满足我们的需求，<b>还可以自定义解析。</b>
+     * 举个例子，假设Book类的isbn是一个BigInteger
+     * <blockquote><pre>
+     *     public class Book {
+     *          public String name;
+     *          public BigInteger isbn;
+     *      }
+     * </pre></blockquote>
+     *
+     * 但JSON数据并不是标准的整形格式：
+     *<blocckquote><pre>
+     *     {
+     *          "name": "Java核心技术",
+     *          "isbn2": "978-7-111-54742-6"
+     *      }
+     *</pre></blocckquote>
+     *
+     * 直接解析，肯定报错。这时，我们需要<b>自定义一个{@link IsbnDeserializer}，用于解析含有非数字的字符串</b><br/>
+     *
+     * 最后，在{@link Book}类isbn2上使用注解：<b>@JsonDeserialize(using = IsbnDeserializer.class)</b>
+     *
+     * @throws IOException
+     */
+    public static void demoOfIsbnDeserializer() throws IOException {
+        InputStream input = JsonSerializationUtil.class.getResourceAsStream("/book.json");
 
-        {
-            "name": "Java核心技术",
-            "isbn2": "978-7-111-54742-6"
-        }
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
 
-        直接解析，肯定报错。这时，我们需要自定义一个IsbnDeserializer，用于解析含有非数字的字符串
+        // 反序列化是忽略不存在的JavaBean属性
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        Book book = mapper.readValue(input, Book.class);
+        System.out.println(book.toString());
 
-        最后，在Book类isbn2上使用注解：@JsonDeserialize(using = IsbnDeserializer.class)
-         */
+        // 序列化
+        String jsonStr = mapper.writeValueAsString(book);
+        System.out.println(jsonStr);
     }
 
 }
